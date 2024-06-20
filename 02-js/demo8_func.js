569c161beb968c789d0e634c20ea0341d418443d
kunkka <kunkka.dai@gmail.com>
2024-06-20 16:06:41 +0800

    add func

--- 02-js/demo8_func.js
+++ 02-js/demo8_func.js
@@ -63,3 +63,84 @@ function sayHi2() {
 function makeKing(name = "Henry") {
   return `King ${name} VIII`;
 }
+
+// 多参数快捷传递的一个例子
+let values = [1, 2, 3, 4];
+function getSum() {
+  let sum = 0;
+  for (let i = 0; i < arguments.length; ++i) {
+    5;
+    sum += arguments[i];
+  }
+  return sum;
+}
+function getSum2(...values) {
+  // 顺序累加 values 中的所有值
+  // 初始值的总和为0
+  return values.reduce((x, y) => x + y, 0);
+}
+
+console.log(getSum(...values)); // 10
+console.log(getSum(-1, ...values)); // 9
+console.log(getSum(...values, 5)); // 15
+console.log(getSum(-1, ...values, 5)); // 14
+console.log(getSum(...values, ...[5, 6, 7])); // 28
+
+/**
+ * =========================函数作为参数===============================
+ */
+function callSomeFunction(someFunction, someArgument) {
+  return someFunction(someArgument);
+}
+function add10(num) {
+  return num + 10;
+}
+console.log(callSomeFunction(add10, 10)); // 20
+
+/**
+ * ============================递归函数=================================
+ */
+function factorial1(num) {
+  if (num <= 1) {
+    return 1;
+  } else {
+    return num * factorial1(num - 1);
+  }
+}
+
+// 更推荐的方式
+const factorial2 = function f(num) {
+  if (num <= 1) {
+    return 1;
+  } else {
+    return num * f(num - 1);
+  }
+};
+
+/**
+ * ==========================函数闭包===============================
+ * 1. 闭包指的是那些引用了另一个函数作用域中变量的函数，通常是在嵌套函数中实现的。允许一个函数访问并操作函数外部的变量。
+ * 2. 闭包在许多方面都非常有用，比如帮助保持变量的私有性，创建部分应用函数和管理状态。任何在块级作用于或者函数内的变量，都有私有的特征。
+ */
+function outer() {
+  let count = 0; // `count` 是 `outer` 函数的局部变量
+  function inner() {
+    count++;
+    console.log(count);
+  }
+  return inner;
+}
+
+// 在这个例子中，inner 函数是一个闭包，因为它访问了其外部函数 outer 的局部变量 count。即使 outer 函数的执行已经完成，count 变量仍然被 inner 函数记住并可以访问和修改。
+const myFunction = outer(); // myFunction成为了一个闭包
+myFunction(); // 输出 1
+myFunction(); // 输出 2
+
+
+/**
+ * =========================立即执行的函数表达式============================
+ * 立即调用的匿名函数又被称作立即调用的函数表达式。
+ */
+(function () {
+  console.log("The End...."); // The End....
+})();
